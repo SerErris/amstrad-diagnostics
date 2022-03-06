d21b2857167ca882385431b96a038ab11c88844f
Christoph Linden <30804693+SerErris@users.noreply.github.com>
2022-03-06 18:49:58 +0100

    Added suport to run Upper ROM from RSX commands

--- src/CPCDiagRomBoard.asm
+++ src/CPCDiagRomBoard.asm
@@ -1,18 +1,21 @@
 MODULE CPCDiagRomBoard
 
 @CPCDiagRomBoardDisableLowerROM:
-	; Disable CPCDiagRomBoard lower ROM so system lower ROM is accessible
+  ; Disable CPCDiagRomBoard lower ROM so system lower ROM is accessible
   ; CPCDiagRomBoard interprets bit7 of the dataword for the Upper ROM select byte.
   ; 0=enabled (default) 1=disabled. Any normal rom select operation will enable rom0 again.
-  ;Deactivate Lower ROM and enables Upper ROM 0
-  LD BC,&DF80            ;output Romselect - bit7 high
-                         ;it also selects Upper Rom 0
-  OUT (C),C              ;output C to &DFxx
+  ;Deactivate Lower ROM and enables Diag Upper ROM
+  LD A,(UpperROMConfig)    ;load the Romnumber of DIAG ROM
+  LD B,&DF                 ;output Romselect
+  LD C,&80                 ;unselect the Lower ROM (bit7 high)
+  OR C                     ;
+  OUT (C),A                ;output C to &DFxx
   ret
 
 @CPCDiagRomBoardEnableLowerROM:
   ;Enables Lower AND Upper ROM0 from the Board.
-  LD BC,&DF00            ;output Romselect - bit7 low.
+  LD C,(UpperRomConfig)  ;load Romnumber of DIAG Rom
+  LD B,&DF               ;output Romselect - bit7 low.
                          ;it also selects Upper Rom 0
   OUT (C),C              ;output C to &DFxx
   ret
