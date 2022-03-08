f1dadff791ac7472dfce80e0f71e0ed923968713
SerErris <christoph.linden@gmx.de>
2022-03-08 20:26:30 +0100

    Added CPCDiagROMCard Support

--- src/CPCDiagRomCard.asm
+++ src/CPCDiagRomCard.asm
@@ -1,20 +1,20 @@
-MODULE CPCDiagRomCard
+ MODULE CPCDiagRomCard
 
 @CPCDiagRomCardDisableLowerROM:
-  ; Disable CPCDiagRomBoard lower ROM so system lower ROM is accessible
-  ; CPCDiagRomBoard interprets bit7 of the dataword for the Upper ROM select byte.
-  ; 0=enabled (default) 1=disabled. Any normal rom select operation will enable rom0 again.
-  ;Deactivate Lower ROM and enables Diag Upper ROM
-  LD BC,&FBEC              ;Address of DiagRom for ROM disable
-  LD A,1                   ;disable rom
-  OUT (C),A                ;output C to &FBEC
-  ret
+	; Disable CPCDiagRomBoard lower ROM so system lower ROM is accessible
+	; CPCDiagRomBoard interprets bit7 of the dataword for the Upper ROM select byte.
+	; 0=enabled (default) 1=disabled. Any normal rom select operation will enable rom0 again.
+	;Deactivate Lower ROM and enables Diag Upper ROM
+	LD BC,#FBEC              ;Address of DiagRom for ROM disable
+	LD A,1                   ;disable rom
+	OUT (C),A                ;output C to &FBEC
+	ret
 
 @CPCDiagRomCardEnableLowerROM:
-  ;Enables Lower AND Upper ROM0 from the Board.
-  LD BC,&FBEC              ;Address of DiagRom for ROM disable
-  LD A,0                   ;enable rom
-  OUT (C),A                ;output A to &FBEC
-  ret
+	;Enables Lower AND Upper ROM0 from the Board.
+	LD BC,#FBEC              ;Address of DiagRom for ROM disable
+	LD A,0                   ;enable rom
+	OUT (C),A                ;output A to &FBEC
+	ret
   
-ENDMODULE
+ ENDMODULE
