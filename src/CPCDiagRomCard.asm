78c266841c5788fe6e62f1d110b27f575f0c2f5e
Christoph Linden <30804693+SerErris@users.noreply.github.com>
2022-03-08 19:47:21 +0100

    Change to new addressing scheme

--- src/CPCDiagRomCard.asm
+++ src/CPCDiagRomCard.asm
@@ -5,19 +5,16 @@ MODULE CPCDiagRomCard
   ; CPCDiagRomBoard interprets bit7 of the dataword for the Upper ROM select byte.
   ; 0=enabled (default) 1=disabled. Any normal rom select operation will enable rom0 again.
   ;Deactivate Lower ROM and enables Diag Upper ROM
-  LD A,(UpperROMConfig)    ;load the Romnumber of DIAG ROM
-  LD B,&DF                 ;output Romselect
-  LD C,&80                 ;unselect the Lower ROM (bit7 high)
-  OR C                     ;
-  OUT (C),A                ;output C to &DFxx
+  LD BC,&FBEC              ;Address of DiagRom for ROM disable
+  LD A,1                   ;disable rom
+  OUT (C),A                ;output C to &FBEC
   ret
 
 @CPCDiagRomCardEnableLowerROM:
   ;Enables Lower AND Upper ROM0 from the Board.
-  LD C,(UpperRomConfig)  ;load Romnumber of DIAG Rom
-  LD B,&DF               ;output Romselect - bit7 low.
-                         ;it also selects Upper Rom 0
-  OUT (C),C              ;output C to &DFxx
+  LD BC,&FBEC              ;Address of DiagRom for ROM disable
+  LD A,0                   ;enable rom
+  OUT (C),A                ;output A to &FBEC
   ret
-
+  
 ENDMODULE
